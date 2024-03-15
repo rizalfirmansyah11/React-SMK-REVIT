88b43cf89af61fd695fdac561847082c71b7a3cf
smkrevit <smkrevit@gamil.com>
2024-03-15 10:03:45 +0700

    Vidio 3

--- src/pages/Sejarah.js
+++ src/pages/Sejarah.js
@@ -3,6 +3,7 @@ function Sejarah() {
       <div className="App">
         <h1>Sejarah SMK REVIT</h1>
         <p>isi sejarah</p>
+        <img src="images/logo.png" alt="logo" srcset="" />
       </div>
     );
   }
