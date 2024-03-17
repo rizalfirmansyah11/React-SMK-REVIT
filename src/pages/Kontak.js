88c3dd8684dbcc6c0410a5833f02206e17f5d343
smkrevit <smkrevit@gamil.com>
2024-03-17 09:18:55 +0700

    Vidio 5

--- src/pages/Kontak.js
+++ src/pages/Kontak.js
@@ -1,8 +1,14 @@
+const css = {
+  color:"red",
+  background:"yellow",
+  fontSize:50
+}
+
 function Kontak() {
     return (
       <div className="App">
-        <h1>Kontak SMK REVIT</h1>
-        <p>Sidoarjo</p>
+        <h1 className="judul">Kontak SMK REVIT</h1>
+        <p style={ css }>Sidoarjo</p>
         
       </div>
     );
